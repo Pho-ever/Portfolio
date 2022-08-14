3a76d0142d9b8db92384b7b70cc0544cd7682c56
Ph0-ever <oluwayomiforever@gmail.com>
2022-08-14 13:57:56 +0100

    creating the landing page

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <div className="landing-container">
+        <h1>
+          hello world
+        </h1>
+
+      </div>
+      <footer>
+        Oluwayomi Forever
+      </footer>
+    </>
+  )
+}
