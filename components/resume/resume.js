b22ffe07cec85fec5917f67206851a69609a458d
Forever Oluwayomi <67446930+Pho-ever@users.noreply.github.com>
2024-06-14 15:35:00 +0100

    fix:bug

--- components/resume/resume.js
+++ components/resume/resume.js
@@ -11,13 +11,11 @@ export default function Resume() {
                     <h3>{constant.name}</h3>
                         <div>
                             <Link href="https://drive.google.com/file/d/195ggmq46hV19pnnPqK_QjJOFfxcqSUtp/view?usp=drive_link">
-                            // <Link href="https://drive.google.com/file/d/18ururDZiN8tq9kf108OkrpLu2p2K2Zh-/view?usp=drive_link">
                                 <a target="_blank">
                                     <Image src='/download.png' width="20%" height="20%" alt="download"/>
                                 </a>
                             </Link>
                             <Link href="https://drive.google.com/file/d/195ggmq46hV19pnnPqK_QjJOFfxcqSUtp/view?usp=drive_link">
-                            // <Link href="https://drive.google.com/file/d/18ururDZiN8tq9kf108OkrpLu2p2K2Zh-/view?usp=drive_link">
                                 <a target="_blank" className="download" >Download</a>
                             </Link>
                         </div>
